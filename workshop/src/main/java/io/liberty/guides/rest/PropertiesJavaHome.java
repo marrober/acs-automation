5c9053636eb6cd65a978dce367385292b9864990
Mark Roberts <marrober@redhat.com>
2021-05-13 15:58:13 +0100

    Update create-tasks.sh, create-trigger-resources.sh, and 29 more files...

---
+++ workshop/src/main/java/io/liberty/guides/rest/PropertiesJavaHome.java
@@ -0,0 +1,26 @@
+// Comment
+
+package io.openliberty.guides.rest;
+
+import java.util.Properties;
+import org.json.simple.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+// oc get route liberty-rest-route -o jsonpath='{"http://"}{.spec.host}{"/System/propertiesJavaHome"}'
+
+@Path("propertiesJavaHome")
+public class PropertiesJavaHome {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProperties() {
+       String javaHome = "Java Home ++++> " + System.getProperty("java.home");
+
+       return(javaHome);
+    }
+
+}
